cdff1d005b0080f4b76940f6a5992ad4db2f5d4a
christian-meyer <christianmeyerxxx@gmail.com>
2025-05-26 14:25:01 -0400

    finished registration ex

---
+++ chap2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace chap2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Student Registration App");
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Year of Birth");
+            int YOB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Welcome {firstName} {lastName}!");
+            Console.WriteLine("Your registration is complete");
+            Console.WriteLine($"Your temporary password is: {firstName}*{YOB}");
+
+            Console.WriteLine("Welcome to the grade converter App");
+
+        }
+    }
+}
