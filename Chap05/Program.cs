447997ee7ea79d01fb87350f4c04c4b9e44043f9
christian-meyer <christianmeyerxxx@gmail.com>
2025-06-19 23:58:07 -0400

    Dice roller App

---
+++ Chap05/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Chap05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the Dice roller app");
+            Console.WriteLine("Roll the dice!? Y/N");
+            string roll = Console.ReadLine();
+            if (roll == "Y")
+            {
+                Random random = new Random();
+            }
+        }
+    }
+}
